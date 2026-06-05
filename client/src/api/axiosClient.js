2dd215f740eadd1d081b61fc1afc1eebe435c1fb
Florin Bighiu <florin.bighiu1@gmail.com>
2026-06-05 18:43:38 +0300

    fix: hardcode Render API URL in axiosClient

--- client/src/api/axiosClient.js
+++ client/src/api/axiosClient.js
@@ -1,7 +1,7 @@
 ﻿import axios from "axios";
 
 const axiosClient = axios.create({
-  baseURL: import.meta.env.VITE_API_URL,
+  baseURL: "https://momentum-fpou.onrender.com",
 });
 
 export default axiosClient;
